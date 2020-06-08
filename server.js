960b7e3db91c3719e7fb28b02b9375df2dadee09
Jack <jack.bucinskas@gmail.com>
2020-06-08 11:21:28 +1000

    Proper practices regarding API Keys

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+/*
+    Jack Bucinskas
+    jb182@uowmail.edu.au
+    5814583
+
+    Group 2 - Project Part II
+ */
+
+// Allow .env files to work
+require('dotenv').config();
+
+// Import Chance Library for Random Test Generation
 var chance = require('chance').Chance();
+
+// Import MongoDB for logging results
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/csit314_db";
+
+// Import Flickr API to conduct Tests
+/*
+    IMPORTANT: Rename .env.example to .env and place your API Key inside OR
+    Replace 'process.env.FLICKR_API_KEY' with your API Key
+ */
 var Flickr = require('flickr-sdk');
-var flickr = new Flickr('365e7e62eb2d307f5af3414068be1000');
+var flickr = new Flickr(process.env.FLICKR_API_KEY);
 
 // Connect to MongoDB
 MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true})
@@ -18,15 +38,3 @@ flickr.photos.getInfo({
     console.error('bonk', err);
 });
 
-
-
-// Limitations if we use Booking.com
-// https://chancejs.com/location/province.html
-// Note, currently support for country is limited to: 'ca', 'it'.
-
-// Limitations to OMDB
-// Very limited Nodejs API, only has a search/get feature for 1 specific movie per query.
-
-// Decide to use Flickr
-// API Key: 365e7e62eb2d307f5af3414068be1000
-// https://github.com/flickr/flickr-sdk
