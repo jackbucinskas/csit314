5823ad8a606d5bb1bb7a5e2f0e0d51dd837e9ba5
Jack <jack.bucinskas@gmail.com>
2020-06-08 10:06:52 +1000

    Flickr API

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-//var mongo = require('mongodb');
 var chance = require('chance').Chance();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/csit314_db";
+var Flickr = require('flickr-sdk');
+var flickr = new Flickr('365e7e62eb2d307f5af3414068be1000');
 
+// Connect to MongoDB
 MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => console.log("Connected to Database"))
     .catch(err => console.error("An error has occured", err));
 
-var country1 = chance.country()
-var state1 = chance.state({ full: true, country: country1})
+// Flickr API
+flickr.photos.getInfo({
+    photo_id: 25825763 // sorry, @dokas
+}).then(function (res) {
+    console.log('yay!', res.body);
+}).catch(function (err) {
+    console.error('bonk', err);
+});
 
-console.log(country1 + " " + state1);
 
-// Limitations
+
+// Limitations if we use Booking.com
 // https://chancejs.com/location/province.html
 // Note, currently support for country is limited to: 'ca', 'it'.
-//var city1 = chance.province({country: country1, full: true});
+
+// Limitations to OMDB
+// Very limited Nodejs API, only has a search/get feature for 1 specific movie per query.
+
+// Decide to use Flickr
+// API Key: 365e7e62eb2d307f5af3414068be1000
+// https://github.com/flickr/flickr-sdk
