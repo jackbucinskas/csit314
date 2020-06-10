ede7be931d73eb85ce89fcacb613abaaaa2f70c4
dieselfuel147 <dieselfuel147@hotmail.com>
2020-06-11 01:17:05 +1000

    taken date test

--- server.js
+++ server.js
@@ -44,58 +44,132 @@ client.connect(function(err) {
     const collection = db.collection('reports');
 
     // Flickr Photo Search API Call
+
+    //Queries - Section 1
     // Randomize search text and print "Searching for: Random Animal"
     var animal_tag = chance.animal().toString();
     console.log("Searching for: " + animal_tag);
 
-    // Query 1
+    var totals = [];
+
+    // Query 1.1
     function query1() {
         flickr.photos.search({
             text: animal_tag,
             min_upload_date: '2020-01-01', // YYYY-MM-DD
             max_upload_date: '2020-02-01'
         }).then(function (res) {
-            console.log('Successful API Call 1', res.body);
+            console.log('Successful API Call 1.1', res.body);
             console.log('Saving to ' + url + dbName + '\n');
+            totals.push(res.body.photos.total); //add total number of results to array
             db.collection("results").insertOne(res.body);
         }).catch(function (err) {
             console.error('Something Went Wrong', err);
         });
     }
 
-    // Query 2
+    // Query 1.2
     function query2() {
         flickr.photos.search({
             text: animal_tag,
             min_upload_date: '2020-01-01',
             max_upload_date: '2020-03-01'
         }).then(function (res) {
-            console.log('Successful API Call 2', res.body);
+            console.log('Successful API Call 1.2', res.body);
             console.log('Saving to ' + url + dbName + '\n');
+            totals.push(res.body.photos.total);
             db.collection("results").insertOne(res.body);
         }).catch(function (err) {
             console.error('Something Went Wrong', err);
         });
     }
 
-    // Query 3
+    // Query 1.3
     function query3() {
         flickr.photos.search({
             text: animal_tag,
             min_upload_date: '2020-01-01',
             max_upload_date: '2020-04-01'
         }).then(function (res) {
-            console.log('Successful API Call 3', res.body);
+            console.log('Successful API Call 1.3', res.body);
             console.log('Saving to ' + url + dbName + '\n');
+            totals.push(res.body.photos.total);
             db.collection("results").insertOne(res.body);
         }).catch(function (err) {
             console.error('Something Went Wrong', err);
         });
     }
 
+    function testQueries1(totalsArray) {
+      var pass; 
+      console.log("--- Test ---");
+      console.log("Is (Query 1 total < Query 2 total < Query 3 total) true?")
+      console.log("Query 1 total =", totalsArray[0]);
+      console.log("Query 2 total =", totalsArray[1]);
+      console.log("Query 3 total =", totalsArray[2]);
+
+      if (totalsArray[0] < totalsArray[1] < totalsArray[2]){
+        pass = true;
+      }
+      else{
+        pass = false;
+      };
+
+      console.log("Section 1 pass:", pass);
+
+      if (posted_date > taken_date){
+        pass = true;
+      } else {
+        pass = false;
+      };
+
+      console.log("Section 2 pass:", pass);
+
+      process.exit();
+    }
+
+
+    //Queries - Section 2
+
+    //Query 2.1
+    var posted_date, taken_date
+
+    function query2_1() {
+      flickr.photos.getRecent({
+        per_page: 1,
+        page: 1,
+        pages: 1
+      }).then(function (res) {
+        console.log("Successful API Call 2.1", res.body);
+        var photo_id = res.body.photos.photo[0].id;
+        console.log("photo id:", photo_id);
+        
+        flickr.photos.getInfo({
+          photo_id: photo_id
+        }).then(function (res) {
+          console.log('Successful getInfo()', res.body);
+          posted_date = res.body.photo.dates.posted;
+          var myDate = new Date(res.body.photo.dates.taken); // Your timezone!
+          taken_date = myDate.getTime()/1000.0;
+          console.log("Epoch date:", taken_date);
+        }).catch(function (err) {
+          console.error('Something Went Wrong', err);
+        });
+        
+      }).catch(function (err) {
+        console.error('Something Went Wrong', err);
+      });
+  }
+
     // Conduct Flickr Search API Call
     query1();
     setTimeout(query2, 1000);
     setTimeout(query3, 2000);
+
+    query2_1();
+
+    //Run test
+    setTimeout(testQueries1, 5000, totals);
+
 
 });
